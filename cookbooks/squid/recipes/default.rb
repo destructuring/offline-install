eb1f24f9562e3a48ae67a46840dcb9ad11bc74d0
Tom Bombadil <amanibhavam@destructuring.org>
2013-09-11 13:08:02 -0700

    still need the vault for squid

--- cookbooks/squid/recipes/default.rb
+++ cookbooks/squid/recipes/default.rb
@@ -7,5 +7,6 @@ end
 template "#{node[:release_dir]}/etc/squid.conf" do
   mode 00600
   source "squid.conf.erb"
-  variables(:squid => "#{node[:release_dir]}")
+  variables(:squid => "#{node[:release_dir]}",
+            :vault => ENV['VAULT'])
 end
